f9fa99d4aa1732b84b4fcacd463ea3124a19c29c
Jack Bonaguro <15988128+jackbonaguro@users.noreply.github.com>
2025-03-07 16:05:47 -0500

    remove firebase secret file

--- index.ts
+++ index.ts
@@ -17,8 +17,6 @@ app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Welcome to the Express + TypeScript Server!" });
 });
 
-const DEVICE_TOKEN_1 = 'fvgVYCa6QLmRQKA3FACWNm:APA91bEU9L83mAmrxVK6nkjHBEY6A1WNbu_mgbaTJzy-Vua4XSn9lIVoSlh46WFPm7P51uiR_T6RTpVRntllwBo2W7r9Qifn_rwKGZOQD-9HExXqmukb5H0';
-const DEVICE_TOKEN_2 = 'enCq4RNYcUsFgA3hpqd79D:APA91bG_Dv-JnA9LiTh6xIvIi66UjHOSkCynECACJK_9sGeGasiTtFb6oTFvU3ujyeinwpjkdeGQDfvKRYix9Qz2c3eS1Aj1iiFrpTO2wUFW2pbIV1oMoeo';
 app.get("/testNotification", async (req: Request, res: Response) => {
   const message = {
     data: {
@@ -32,7 +30,7 @@ app.get("/testNotification", async (req: Request, res: Response) => {
       title: 'Countdown "Nora\'s Birthday" is 1 day away!',
       // body: 'E',
     },
-    token: DEVICE_TOKEN_2
+    token: process.env.DEVICE_TOKEN!
   };
 
   getMessaging().send(message)
