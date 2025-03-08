1f22810c4b19b201bf27cb7025c8be780e650f8b
Jack Bonaguro <15988128+jackbonaguro@users.noreply.github.com>
2025-03-07 20:03:05 -0500

    add register endpoint

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import * as firebase from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
+import { PrismaClient } from '@prisma/client'
 
 const firebaseApp = firebase.initializeApp({
   credential: firebase.applicationDefault(),
 });
 
+const prisma = new PrismaClient();
 // Create a new express application instance
 const app = express();
 
@@ -15,6 +17,35 @@ const port = process.env.PORT || 3000;
 // Define the root path with a greeting message
 app.get("/status", (req: Request, res: Response) => {
     res.json({ status: "ok" });
+});
+
+app.get("/register", async (req: Request, res: Response) => {
+  // Log request headers
+  console.log(req.headers);
+
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const device = await prisma.device.findFirst({
+    where: {
+      token: token
+    }
+  });
+  if (!device) {
+    // Create a new device
+    console.log("Creating new device with token", token);
+    const newDevice = await prisma.device.create({
+      data: {
+        token: token
+      }
+    });
+  } else {
+    console.log("Device already exists with token", token);
+  }
+
+  res.json({ message: "Device registered" });
 });
 
 app.get("/testNotification", async (req: Request, res: Response) => {
